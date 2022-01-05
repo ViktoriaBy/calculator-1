ed36b03449a56477cb42d696243222cc7fc1c306
ViktoriaB <v.chamborant@hotmail.com>
2022-01-04 19:20:31 -0600

    LABWORK USED WRONG INDEXES IN TOKEN ARRAY

--- calc.js
+++ calc.js
@@ -10,13 +10,10 @@ const reader = readline.createInterface({
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
+	num1 = Number(tokens[0]);
+	mathSymbol = tokens[1];
 	num2 = Number(tokens[2]);
 
-	console.log('mathSymbol', mathSymbol);
-	console.log('num1', num1);
-	console.log('num2', num2);
 
 	if (mathSymbol === "+"){
 		console.log(num1 + num2);
